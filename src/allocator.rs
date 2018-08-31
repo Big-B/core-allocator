0d6aeb405a4ed6216ae5170e213afb3a9cda2286
Brandon Mittman <BrandonMittman@gmail.com>
2018-08-30 19:00:15 -0700

    Make threadsafe

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,13 +1,19 @@
 use core::Core;
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
+use std::sync::Mutex;
 
 pub struct Allocator {
-    cores: BinaryHeap<Core>,
-    mappings: HashMap<String, usize>,
+    cores: Mutex<BinaryHeap<Core>>,
+    mappings: Mutex<HashMap<String, usize>>,
 }
 
 impl Allocator {
+    /// Creates a core allocator which will distribute to threads the cores
+    /// which have been passed in.
+    /// ```
+    /// let mut allocator = Allocator::new(vec![1,2,3]);
+    /// ```
     pub fn new(mut cores_list: Vec<usize>) -> Self {
         // Make sure there are no duplicates
         cores_list.sort_unstable();
@@ -18,16 +24,20 @@ impl Allocator {
 
         // Return Allocator
         Allocator {
-            cores: map,
-            mappings: HashMap::new(),
+            cores: Mutex::new(map),
+            mappings: Mutex::new(HashMap::new()),
         }
     }
 
+    /// Get a core for a thread. If the same `thread_name` is passed in more than once
+    /// then it will get the same core.
     pub fn get_core_for_thread(&mut self, thread_name: &str, priority: usize) -> usize {
+        let mut map = self.mappings.lock().unwrap();
+        let mut heap = self.cores.lock().unwrap();
         // Check to see if we already serviced this thread
-        *self.mappings.entry(thread_name.to_owned()).or_insert(
+        *map.entry(thread_name.to_owned()).or_insert(
             // We haven't serviced this thread, get a core and add to map
-            if let Some(mut core) = self.cores.pop() {
+            if let Some(mut core) = heap.pop() {
                 // Get the core number
                 let core_num = core.get_core_num();
 
@@ -35,7 +45,7 @@ impl Allocator {
                 core.add_thread(thread_name, priority);
 
                 // Return core to heap
-                self.cores.push(core);
+                heap.push(core);
 
                 // Give back the core number
                 core_num
@@ -47,14 +57,18 @@ impl Allocator {
         )
     }
 
+    /// Remove a thread from a core.
     pub fn remove_thread_from_core(&mut self, thread_name: &str) {
+        let mut map = self.mappings.lock().unwrap();
         // Check to see if thread has been added already
-        if let Some(core_num) = self.mappings.remove(thread_name) {
+        if let Some(core_num) = map.remove(thread_name) {
             // Create a temporary heap to move cores to
             let mut temp = BinaryHeap::new();
 
+            let mut heap = self.cores.lock().unwrap();
+
             // Search for the right core to remove the thread from
-            while let Some(mut core) = self.cores.pop() {
+            while let Some(mut core) = heap.pop() {
                 // Core num needs to match
                 if core.get_core_num() == core_num {
                     // Remove the thread
@@ -64,7 +78,7 @@ impl Allocator {
                 temp.push(core);
             }
             // Place updated cores in our heap
-            self.cores.append(&mut temp);
+            heap.append(&mut temp);
         }
     }
 }
